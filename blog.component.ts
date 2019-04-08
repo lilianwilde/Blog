aa3180a8bbf8901a16d2ff62cfe7bd02bd2268fd
lilianwilde <37550145+lilianwilde@users.noreply.github.com>
2019-04-08 17:53:58 -0300

    Add files via upload

---
+++ blog.component.ts
@@ -0,0 +1,40 @@
+import { Component, NgModule, style } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import { ValueUnwrapper } from '@angular/core/src/change_detection/change_detection';
+
+
+@Component({
+    selector: 'blog',
+    template: 
+'<div class="form-group" *ngFor= "let post of blogPosts">
+    <textarea class="form" rows="7"></textarea>
+    <input class="btn btn-primary" type="submit" value="Submit">
+</div>'
+
+})
+
+
+export class BlogComponent {
+        
+    blogPost: Object []; 
+
+    constructor () {
+      this.blogPost = [
+        {  
+            id:1,
+            texto:"Teste1 teste teste",
+            datahora:new Date ("2019-04-04T11:25:43.511Z"). toLocaleString()
+         }, {  
+            id:2,
+            texto:"Teste 2 teste  teste ",
+            datahora:new Date ("2019-04-04T12:25:43.511Z"). toLocaleString()
+         }, 
+         {  
+            id:3,
+            texto:"Teste 3 teste  teste ",
+            datahora:new Date ("2019-04-04T12:25:43.511Z"). toLocaleString()
+         }
+      ]
+    }
+}
